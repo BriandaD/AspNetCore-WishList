a577d862ef8340bb2733d1f09fd0fc5301dc30ee
Brianda Delgado <brianda.delgado@softtek.com>
2019-08-27 16:57:01 -0700

    Update HomeController.cs

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -10,12 +10,12 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Error()
         {
-            return RedirectToPage("../Views/Shared/Error");
+            return View("Error");
         }
     }
 }
